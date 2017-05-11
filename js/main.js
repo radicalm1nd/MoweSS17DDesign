82200ead0011c886135e7777e45e7fb8d960fa42
Dustin Herzog <dustin.herzog@googlemail.com>
2017-05-11 13:51:20 +0200

    Added main.js and first script

---
+++ js/main.js
@@ -0,0 +1,177 @@
+$( document ).ready(function() {
+	
+	if (window.location.href.indexOf("index.html") > -1) { //if mainpage
+	$("#mainRecipes").html(" ");
+    $.getJSON( "data/beispielrezept.json", function( data ) {
+	for (i=0; i<data.length; i++) {
+		var imageURL = data[i].image;
+		var imageName = data[i].name;
+		$("#mainRecipes").append("
+		<div class='column is-3'>
+		        <div class='card'>
+          <div class='card-image'>
+            <figure class='image is-4by3'>
+              <img id="cardIMG">
+            </figure>
+          </div>
+        </div>
+      </div>
+		");
+		$("#cardIMG").attr("src", imageURL);
+		$("#cardIMG").attr("alt", imageName);
+	}
+	});
+};
+});
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*
+	
+        <div class="card">
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <img src="img/placeholder.jpg" alt="Image">
+            </figure>
+          </div>
+        </div>
+      </div>
+	        <div class="column is-3">
+        <div class="card">
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <img src="img/placeholder.jpg" alt="Image">
+            </figure>
+          </div>
+        </div>
+      </div>
+	        <div class="column is-3">
+        <div class="card">
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <img src="https://cdn.shopify.com/s/files/1/1267/5363/products/JUNGLE-FEVER-E-JUICE-BY-rUTHLESS-VAPOR.jpg?v=1481571554" alt="Image">
+            </figure>
+          </div>
+        </div>
+      </div>
+	        <div class="column is-3">
+        <div class="card">
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <img src="img/placeholder.jpg" alt="Image">
+            </figure>
+          </div>
+        </div>
+      </div>
+	 </div>
+	     <div class="columns">
+      <div class="column is-3">
+        <div class="card">
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <img src="img/placeholder.jpg" alt="Image">
+            </figure>
+          </div>
+        </div>
+      </div>
+	        <div class="column is-3">
+        <div class="card">
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <img src="img/placeholder.jpg" alt="Image">
+            </figure>
+          </div>
+        </div>
+      </div>
+	        <div class="column is-3">
+        <div class="card">
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <img src="img/placeholder.jpg" alt="Image">
+            </figure>
+          </div>
+        </div>
+      </div>
+	        <div class="column is-3">
+        <div class="card">
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <img src="img/placeholder.jpg" alt="Image">
+            </figure>
+          </div>
+        </div>
+      </div>
+	 </div>
+	     <div class="columns">
+      <div class="column is-3">
+        <div class="card">
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <img src="img/placeholder.jpg" alt="Image">
+            </figure>
+          </div>
+        </div>
+      </div>
+	        <div class="column is-3">
+        <div class="card">
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <img src="img/placeholder.jpg" alt="Image">
+            </figure>
+          </div>
+        </div>
+      </div>
+	        <div class="column is-3">
+        <div class="card">
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <img src="img/placeholder.jpg" alt="Image">
+            </figure>
+          </div>
+        </div>
+      </div>
+	        <div class="column is-3">
+        <div class="card">
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <img src="img/placeholder.jpg" alt="Image">
+            </figure>
+          </div>
+        </div>
+      </div>
+	
+	*/
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
