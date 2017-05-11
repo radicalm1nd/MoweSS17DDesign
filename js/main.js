be5de7d9f208745a9d26ca884fa4c7e6e41bd1f5
Dustin Herzog <dustin.herzog@googlemail.com>
2017-05-11 14:54:45 +0200

    success

--- js/main.js
+++ js/main.js
@@ -6,15 +6,13 @@ $( document ).ready(function() {
     $.getJSON( "data/beispielrezept.json", function() {
 		console.log( "success" );
 	})
-		.done(function(data) {
+		.success(function(data) {
 				for (i=0;data.length; i++) {
 		imageURL = data[i].image;
 		imageName = data[i].name;
 		$("#mainRecipes").append("<div class='column is-3'><div class='card'><div class='card-image'><figure class='image is-4by3'><img id='cardIMG'></figure></div></div></div>")
 		$("#cardIMG").attr("src", imageURL[i]);
 		$("#cardIMG").attr("alt", imageName[i]);
-		console.log(imageURL);
-		console.log(imageName);
 		if (i%4 == 0) {
 			 $("#mainRecipes").append("</div><div class='columns'>");
 		} 
