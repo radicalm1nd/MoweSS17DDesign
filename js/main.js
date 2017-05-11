06ae3a9cb871be163b6a2f1630731807284e4a9d
Dustin Herzog <dustin.herzog@googlemail.com>
2017-05-11 15:16:15 +0200

    fck me

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ $( document ).ready(function() {
 		$.each(data, function (i, item) {
 			imageURL = data[i].image;
 			imageName = data[i].name;
-			$("#mainRecipes").append("<div class='column is-3'><div class='card'><div class='card-image'><figure class='image is-4by3'><img id='cardIMG'></figure></div></div></div>")
+			$("#mainRecipes").append("<div class='column is-3'><div class='card'><div class='card-image'><figure class='image is-4by3'><img id='cardIMG' src='"+imageURL+"'></figure></div></div></div>")
 			$("#cardIMG").attr("src", imageURL);
 			$("#cardIMG").attr("alt", imageName);
 			if (counter%4 == 0) {
